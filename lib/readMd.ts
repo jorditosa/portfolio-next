c17f6423b713aabd481c98145b3ce041a11381c9
jorditosa <jordisato88@gmail.com>
2024-07-03 16:17:22 +0200

    fixing fetch

--- lib/readMd.ts
+++ lib/readMd.ts
@@ -1,9 +1,13 @@
-import { marked } from 'marked'
-import { readFile } from 'node:fs/promises'
+import { marked } from 'marked';
+import { readFile } from 'node:fs/promises';
 
 export async function getTexts(slug: string) {
-  const text = await readFile(`public/content/${slug}.md`, 'utf-8')
-  const html = marked.parse(text)
-  return html
+  try {
+    const text = await readFile(`public/content/${slug}.md`, 'utf-8');
+    const html = marked.parse(text);
+    return html;
+  } catch (error) {
+    console.error(`Error reading or parsing file for slug "${slug}":`, error);
+    throw new Error(`Unable to get content for ${slug}`);
+  }
 }
-
