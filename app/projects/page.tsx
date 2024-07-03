16bcb02199781bd74af341cc9ef73f3e3a2b5276
jorditosa <jordisato88@gmail.com>
2024-07-03 14:33:40 +0200

    counterpart client

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import { AiOutlineLaptop, AiOutlineMobile, AiOutlineCode } from "react-icons/ai";
+
 import Heading from "../../components/Heading";
+import ProjectList from "./ui/ProjectList";
 
 
 export default async function ProjectsPage() {
@@ -8,40 +8,9 @@ export default async function ProjectsPage() {
     <div>
       <Heading title="Projects" backBtn={true} />
 
-      <ul>
-        <Heading title="Fullstack Apps" backBtn={false} />
-        <li className="text-xl">
-          <Link href={`/projects/deporunners`} className="flex items-center gap-6 no-underline hover:text-blue-700">
-            <AiOutlineMobile className="w-8 h-8" />
-            Deporunners App
-          </Link>
-        </li>
-        <li className="text-xl">
-          <Link href={`/projects/takespai`} className="flex items-center gap-6 no-underline hover:text-blue-700">
-            <AiOutlineLaptop className="w-8 h-8" />
-            Takespai Ecommerce
-          </Link>
-        </li>
-        <li className="text-xl">
-          <Link href={`/projects/cursacolls`} className="flex items-center gap-6 no-underline hover:text-blue-700">
-            <AiOutlineLaptop className="w-8 h-8" />
-            Cursa dels Colls Website
-          </Link>
-        </li>
+      <ProjectList />
 
-      </ul>
-
-      <ul>
-        <Heading title="Personal Scripts" backBtn={false} />
-
-
-        <li className="text-xl">
-          <Link href={`/projects/network-analyzer`} className="flex items-center gap-6 no-underline hover:text-blue-700">
-            <AiOutlineCode className="w-8 h-8" />
-            Python Network analyzer
-          </Link>
-        </li>
-      </ul>
+     
     </div>
   )
 }
