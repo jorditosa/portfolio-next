5b838b99832d7ebd76c970a3affa757685a05fd8
jorditosa <jordisato88@gmail.com>
2024-07-03 16:10:39 +0200

    navbar

--- app/ui/navbar/NavBar.tsx
+++ app/ui/navbar/NavBar.tsx
@@ -11,48 +11,48 @@ export default async function NavBar() {
   return (
     <RoundedNav>
       <ul className="w-full h-48 flex font-semibold text-xs lg:text-xl overflow-hidden">
-        <li className="py-2 flex-1 cursor-pointer gap-12 text-lg bg-gradient-to-t from-blue-500 dark:to-slate-950 hover:text-xl transition-all duration-100 ease-linear">
-          <Link href='/' className='h-full flex flex-col items-center justify-end gap-12'>
+        <li className="relative py-2 flex-1 cursor-pointer gap-8 bg-gradient-to-t from-blue-500 dark:to-slate-950 before:absolute before:bg-blue-900 before:w-full before:h-full before:-top-[100%] before:duration-200 before:ease-in transition-all before:hover:top-0 before:z-0 hover:text-xl hover:text-white duration-100 ease-linear">
+          <Link href='/' className='h-full flex flex-col items-center justify-end gap-8'>
             <p className='-rotate-6'>Hello!</p>
-            <AiOutlineHome size={30} />
+            <AiOutlineHome size={42} className="z-20" />
           </Link>
 
         </li>
-        <li className="py-2 flex-1 cursor-pointer gap-12 text-lg bg-gradient-to-t from-blue-500/80 dark:to-slate-950 hover:text-xl transition-all duration-100 ease-linear">
-          <Link href='/background' className='h-full flex flex-col items-center justify-end gap-12'>
+        <li className="relative py-2 flex-1 cursor-pointer gap-8 bg-gradient-to-t from-blue-500/80 dark:to-slate-950 before:absolute before:bg-blue-900 before:w-full before:h-full before:-top-[100%] before:duration-200 before:ease-in transition-all before:hover:top-0 before:z-0 hover:text-xl hover:text-white duration-100 ease-linear">
+          <Link href='/background' className='h-full flex flex-col items-center justify-end gap-8'>
             <p className='-rotate-6'>Career</p>
-            <AiOutlineLaptop size={30} />
+            <AiOutlineLaptop size={42} className="z-20" />
           </Link>
         </li>
-        <li className="py-2 flex-1 cursor-pointer gap-12 text-lg bg-gradient-to-t from-blue-500/70 dark:to-slate-950 hover:text-xl transition-all duration-100 ease-linear">
-          <Link href='./studies' className='h-full flex flex-col items-center justify-end gap-12'>
+        <li className="relative py-2 flex-1 cursor-pointer gap-8 bg-gradient-to-t from-blue-500/60 dark:to-slate-950 before:absolute before:bg-blue-900 before:w-full before:h-full before:-top-[100%] before:duration-200 before:ease-in transition-all before:hover:top-0 before:z-0 hover:text-xl hover:text-white duration-100 ease-linear">
+          <Link href='./studies' className='h-full flex flex-col items-center justify-end gap-8'>
             <p className='-rotate-6'>Studies</p>
-            <AiOutlineRead size={30} />
+            <AiOutlineRead size={42} className="z-20" />
           </Link>
         </li>
-        <li className="py-2 flex-1 cursor-pointer gap-12 text-lg bg-gradient-to-t from-blue-500/60 dark:to-slate-950 hover:text-xl transition-all duration-100 ease-linear">
-          <Link href='/projects' className='h-full flex flex-col items-center justify-end gap-12'>
+        <li className="relative py-2 flex-1 cursor-pointer gap-8 bg-gradient-to-t from-blue-500/40 dark:to-slate-950 before:absolute before:bg-blue-900 before:w-full before:h-full before:-top-[100%] before:duration-200 before:ease-in transition-all before:hover:top-0 before:z-0 hover:text-xl hover:text-white duration-100 ease-linear">
+          <Link href='/projects' className='h-full flex flex-col items-center justify-end gap-8'>
             <p className='-rotate-6'>Projects</p>
-            <AiOutlineSnippets size={30} />
+            <AiOutlineSnippets size={42} className="z-20" />
           </Link>
         </li>
         {/* {
           session ?
-            <li className="py-2 flex-1 cursor-pointer gap-12 text-lg bg-gradient-to-t from-slate/50 dark:to-slate-950 hover:text-xl transition-all duration-100 ease-linear">
-              <Link href='/api/auth/signout' className='h-full flex flex-col items-center justify-end gap-12'>
+            <li className=" relativepy-2 flex-1 cursor-pointer gap-8 bg-gradient-to-t from-slate/50 dark:to-slate-950 before:absolute before:bg-blue-900 before:w-full before:h-full before:-top-[100%] before:duration-200 before:ease-in transition-all before:hover:top-0 before:z-0 hover:text-xl hover:text-white duration-100 ease-linear">
+              <Link href='/api/auth/signout' className='h-full flex flex-col items-center justify-end gap-8'>
                 <p className='-rotate-6'>Logout</p>
-                <AiOutlineLogout size={30} />
+                <AiOutlineLogout size={42} className="z-20" />
               </Link>
             </li>
             :
-            <li className="py-2 flex-1 cursor-pointer gap-12 text-lg bg-gradient-to-t from-slate/50 dark:to-slate-950 hover:text-xl transition-all duration-100 ease-linear">
-              <Link href='/api/auth/signin' className='h-full flex flex-col items-center justify-end gap-12'>
+            <li className=" relativepy-2 flex-1 cursor-pointer gap-8 bg-gradient-to-t from-slate/50 dark:to-slate-950 before:absolute before:bg-blue-900 before:w-full before:h-full before:-top-[100%] before:duration-200 before:ease-in transition-all before:hover:top-0 before:z-0 hover:text-xl hover:text-white duration-100 ease-linear">
+              <Link href='/api/auth/signin' className='h-full flex flex-col items-center justify-end gap-8'>
                 <p className='-rotate-6'>Login</p>
-                <AiOutlineLogin size={30} />
+                <AiOutlineLogin size={42} className="z-20" />
               </Link>
             </li>
         } */}
-        <li className="h-full py-2 flex-1 gap-4 cursor-pointer text-lg bg-gradient-to-t from-blue-500/25 dark:to-slate-950 hover:text-xl transition-all duration-100 ease-linear flex flex-col md:flex-row items-center justify-end md:justify-center">
+        <li className="h-full py-2 flex-1 gap-4 cursor-pointer bg-gradient-to-t from-blue-500/25 dark:to-slate-950 hover:text-xl duration-100 ease-linear flex flex-col md:flex-row items-center justify-end md:justify-center">
           <ThemeToggle />
           <LanguageToggle />
         </li>
