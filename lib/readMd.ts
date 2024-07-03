088e3e98167cdc89a8f393b9efc9a8da0e025f0b
jorditosa <jordisato88@gmail.com>
2024-07-03 16:38:38 +0200

    chang env

--- lib/readMd.ts
+++ lib/readMd.ts
@@ -1,9 +1,17 @@
+'use server'
+
 import { marked } from 'marked';
 import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
 
 export async function getTexts(slug: string) {
+  const env = process.env.NODE_ENV
+  const route = env === 'development' ? 'public/content' : 'content'
+
+  const file_path = join(process.cwd(), route, `/${slug}.md`)
+
   try {
-    const text = await readFile(`public/content/${slug}.md`, 'utf-8');
+    const text = await readFile(file_path, 'utf-8');
     const html = marked.parse(text);
     return html;
   } catch (error) {
